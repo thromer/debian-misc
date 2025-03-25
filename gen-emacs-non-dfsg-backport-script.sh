b160d0343732a499b08fb0c70f14e7cb1890676f
Ted Romer <tromer@gmail.com>
2025-03-25 13:32:29 -0700

    Just use backports and live with the old-school quotation marks. Also fix a typo in the line adding to sources.list.

--- gen-emacs-non-dfsg-backport-script.sh
+++ gen-emacs-non-dfsg-backport-script.sh
@@ -5,9 +5,9 @@
 # sudo sbuild-adduser ${USER}
 
 
-WORKDIR=${HOME}/build-backports
 RELEASE=bookworm
 PACKAGE=emacs-non-dfsg
+WORKDIR=${HOME}/build-backports/${PACKAGE}
 DCH_COMMENT=""    # Or something more interesting
 
 COMPONENT=non-free
@@ -15,16 +15,17 @@ SBUILD_DIR_SUFFIX=""
 SBUILD_TARGET_DIR="${RELEASE}-backports${SBUILD_DIR_SUFFIX}"
 SBUILD_EXTRA_REPOS=(
     "deb http://deb.debian.org/debian ${RELEASE}-backports main" 
-    "deb http://deb.debian.org/debian testing main"
+#    "deb http://deb.debian.org/debian testing main"
 )
 SBUILD_EXTRA_PACKAGES=(
     msmtp-mta
     emacs-el/bookworm-backports
     emacs-nox/bookworm-backports
-    texinfo-lib/testing
-    texinfo/testing
+#    texinfo-lib/testing
+#    texinfo/testing
 )
 VERSION=$(curl -s "https://api.ftp-master.debian.org/madison?package=emacs-non-dfsg&s=testing&f=json" | jq -r '.[0]|to_entries[0].value|to_entries[0].value|to_entries[0].key' | perl -p -e 's@[0-9]+:@@')
+PACKAGE_DIR="${PACKAGE}-$(echo ${VERSION} | perl -p -e 's@-[0-9]+$@@')"
 
 LAST_VERSION=${VERSION}   # might be trickier if there were changelog entries from past backport versions.
 
@@ -33,13 +34,23 @@ URL="http://deb.debian.org/debian/pool/${COMPONENT}/$(echo $PACKAGE|cut -c 1)/${
 cat <<EOF
 #!/bin/bash -e
 
-if [[ ("\${DEBEMAIL}" == "") || ("\${DEBFULLNAME}" == "") ]] ;
-  then echo Please set DEBEMAIL and DEBFULLNAME
+if [[ ("\${DEBEMAIL}" == "") || ("\${DEBFULLNAME}" == "") ]] ; then
+  echo Please set DEBEMAIL and DEBFULLNAME
+  fail=1
+fi
+if [[ -e "${WORKDIR}" ]] ; then
+  echo ${WORKDIR} is in the way
+  fail=1
+fi
+if [[ -e "/srv/chroot/${SBUILD_TARGET_DIR}" ]] ; then
+  echo /srv/chroot/${SBUILD_TARGET_DIR} is in the way. For assistance: run sbuild-destroychroot ${SBUILD_TARGET_DIR}
+  fail=1
+fi
+if [[ \$fail ]] ; then
   exit 1
 fi
 
-mkdir -p "${WORKDIR}/${PACKAGE}"
-cd "${WORKDIR}/${PACKAGE}"
+mkdir -p "${WORKDIR}" && cd "${WORKDIR}"
 dget "$URL"
 cd "${PACKAGE}-$(echo ${VERSION} | perl -p -e 's@-[0-9]+$@@')"
 dch --bpo "${DCH_COMMENT}"
@@ -48,7 +59,7 @@ sudo sbuild-createchroot --chroot-prefix=${RELEASE}-backports ${RELEASE} /srv/ch
 EOF
 for REPO in "${SBUILD_EXTRA_REPOS[@]}"; do
     cat <<EOF
-echo "'${REPO}' | cat >> /etc/apt/sources.list" | sudo sbuild-shell ${SBUILD_TARGET_DIR}
+echo "echo '${REPO}' | cat >> /etc/apt/sources.list" | sudo sbuild-shell ${SBUILD_TARGET_DIR}
 EOF
 done
 
